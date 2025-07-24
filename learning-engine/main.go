958a9d47c5e442b6df46e1bf5d52f470b4b4136a
Alexey Pustovit <xieeed@gmail.com>
2025-07-24 20:50:08 +0000

    Add dummy services with base http servers

---
+++ learning-engine/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+const serviceName = "learning-engine"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("'PORT' env var is not set")
+		return
+	}
+
+	srv := gin.Default()
+	srv.GET("/ping", func(c *gin.Context) {
+		c.String(200, fmt.Sprintf("%s is alive", serviceName))
+	})
+
+	srv.Run(fmt.Sprintf(":%s", port))
+}
